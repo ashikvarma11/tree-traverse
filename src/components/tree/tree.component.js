7486fccd04d779d8375c17272acb1c9570298f3b
Omri <omri206attiya@gmail.com>
2020-10-04 09:22:25 +0300

    update header

--- src/components/tree/tree.component.js
+++ src/components/tree/tree.component.js
@@ -1,127 +1,143 @@
-import React, { useContext, useState } from 'react';
+import React, {useContext, useState} from 'react';
 import TreeModel from '../../classes/tree.model';
 import Node from './subtree/node/node.component';
 import './tree.component.css';
 import TreeContext from './tree_context';
+import logo from './logo.svg';
+
 function Tree() {
-  let treeContext = useContext(TreeContext);
-  const [subtrees, setSubtree] = useState(treeContext.subtrees);
-  const [currNode, setCurrNode] = useState(0);
-  const [removeKey, setRemoveKey] = useState(0);
-  const [insertKey, setInsertKey] = useState(0);
-  const [value, setValue] = useState(0);
-  const [treeNode, setTreeNode] = useState(new TreeModel());
-  const [updateTree, setUpdateTree] = useState(0); // integer state
-  let nodeList = [];
-  let insertedKey = 0;
+    let treeContext = useContext(TreeContext);
+    const [subtrees, setSubtree] = useState(treeContext.subtrees);
+    const [currNode, setCurrNode] = useState(0);
+    const [removeKey, setRemoveKey] = useState(0);
+    const [insertKey, setInsertKey] = useState(0);
+    const [value, setValue] = useState(0);
+    const [treeNode, setTreeNode] = useState(new TreeModel());
+    const [updateTree, setUpdateTree] = useState(0); // integer state
+    let nodeList = [];
+    let insertedKey = 0;
 
-  const insertStuff = () => {
-    treeNode.insert(parseInt(insertKey), parseInt(value));
-    insertedKey = parseInt(insertKey);
-    setUpdateTree(value => ++value); // update the state to force render
+    const insertStuff = () => {
+        treeNode.insert(parseInt(insertKey), parseInt(value));
+        insertedKey = parseInt(insertKey);
+        setUpdateTree(value => ++value); // update the state to force render
 
-  }
+    }
 
-  const removeStuff = () => {
-    treeNode.remove(parseInt(removeKey));
-    setUpdateTree(value => ++value); // update the state to force render
+    const removeStuff = () => {
+        treeNode.remove(parseInt(removeKey));
+        setUpdateTree(value => ++value); // update the state to force render
 
-  }
+    }
 
 
-  const constructTree = (node) => {
+    const constructTree = (node) => {
 
-    if (node != null) {
-      return <div className="subtree">
-        <Node key={node.key} id={node.key} value={node.value} />
+        if (node != null) {
+            return <div className="subtree">
+                <Node key={node.key} id={node.key} value={node.value}/>
 
-        {node.left && !node.right && <div className={`children left`}>
-          {constructTree(node.left)}
-        </div>
+                {node.left && !node.right && <div className={`children left`}>
+                    {constructTree(node.left)}
+                </div>
+                }
+                {!node.left && node.right && <div className={`children right`}>
+                    {constructTree(node.right)}
+                </div>
+                }
+
+                {node.left && node.right && <div className={`children`}>
+                    {constructTree(node.left)}
+                    {constructTree(node.right)}
+                </div>
+                }
+
+            </div>
         }
-        {!node.left && node.right && <div className={`children right`}>
-          {constructTree(node.right)}
-        </div>
+    }
+    const calculateTraversal = (node) => {
+        nodeList.push(node);
+    }
+
+    const showAnimation = () => {
+        function task(node, i) {
+            setTimeout(function () {
+                treeContext.curr_node_idx = node.key;
+                setCurrNode(node.key);
+            }, 1000 * i);
         }
 
-        {node.left && node.right && <div className={`children`}>
-          {constructTree(node.left)}
-          {constructTree(node.right)}
-        </div>
+        for (let i = 0; i < nodeList.length; i++) {
+            task(nodeList[i], i);
+        }
+    }
+    const traversal = (type) => {
+        nodeList = [];
+        switch (type) {
+            case 0:
+                treeNode.inorder(treeNode.root, calculateTraversal);
+                break;
+            case 1:
+                treeNode.preorder(treeNode.root, calculateTraversal);
+                break;
+            case 2:
+                treeNode.postorder(treeNode.root, calculateTraversal);
+                break;
+            case 3:
+                treeNode.breadthFirst(treeNode.root, calculateTraversal);
+                break;
+            case 4:
+                treeNode.depthFirst(treeNode.root, calculateTraversal);
+                break;
+            default:
+                return;
         }
 
-      </div>
-    }
-  }
-  const calculateTraversal = (node) => {
-    nodeList.push(node);
-  }
-
-  const showAnimation = () => {
-    function task(node, i) {
-      setTimeout(function () {
-        treeContext.curr_node_idx = node.key;
-        setCurrNode(node.key);
-      }, 1000 * i);
+        showAnimation();
     }
 
-    for (let i = 0; i < nodeList.length; i++) {
-      task(nodeList[i], i);
-    }
-  }
-  const traversal = (type) => {
-    nodeList = [];
-    switch (type) {
-      case 0: treeNode.inorder(treeNode.root, calculateTraversal); break;
-      case 1: treeNode.preorder(treeNode.root, calculateTraversal); break;
-      case 2: treeNode.postorder(treeNode.root, calculateTraversal); break;
-      case 3: treeNode.breadthFirst(treeNode.root, calculateTraversal); break;
-      case 4: treeNode.depthFirst(treeNode.root, calculateTraversal); break;
-      default: return;
-    }
+    return (
+        <div className="container">
+            <div className="header">
+                <div className="logo-bar">
+                    <img className="logo" src={logo} width={50}/>
+                    <div id="title">BINARY SEARCH TREE VISUALIZATION</div>
+                    <img className="logo" src={logo} width={50}/>
+                </div>
+                <div className="navbar">
+                    <div className="key">
+                        Key <input type="number" name="key" onChange={(e) => setInsertKey(e.target.value)}/>
+                        <button onClick={() => insertStuff()}>Insert</button>
+                    </div>
 
-    showAnimation();
-  }
-
-  return (
-
-    <div className="container">
-      <div className="navbar">
-        <div className="logo">
-          <h6>BINARY SEARCH TREE</h6>
-        </div>
-        <div className="key">
-          Key <input type="number" name="key" onChange={(e) => setInsertKey(e.target.value)} />
-          <button onClick={() => insertStuff()}>Insert</button>
-        </div>
-
-        {/* Out of Scope for now */}
-        {/* <div>
+                    {/* Out of Scope for now */}
+                    {/* <div>
         Value <input type="number" placeholder="value" onChange={(e)=>setValue(e.target.value)}/>
         </div> */}
 
 
-        <button onClick={() => traversal(0)}>In-Order traversal</button>
-        <button onClick={() => traversal(1)}>Pre-Order traversal</button>
-        <button onClick={() => traversal(2)}>Post-Order traversal</button>
-        <button onClick={() => traversal(3)}>Breadth-First traversal</button>
-        <button onClick={() => traversal(4)}>Depth-First traversal</button>
+                    <button onClick={() => traversal(0)}>In-Order traversal</button>
+                    <button onClick={() => traversal(1)}>Pre-Order traversal</button>
+                    <button onClick={() => traversal(2)}>Post-Order traversal</button>
+                    <button onClick={() => traversal(3)}>Breadth-First traversal</button>
+                    <button onClick={() => traversal(4)}>Depth-First traversal</button>
 
-        <div className="key">
-          <input type="number" placeholder="Remove key" onChange={(e) => setRemoveKey(e.target.value)} />
-          <button onClick={() => removeStuff()}>Remove</button>
+                    <div className="key">
+                        <input type="number" placeholder="Remove key" onChange={(e) => setRemoveKey(e.target.value)}/>
+                        <button onClick={() => removeStuff()}>Remove</button>
+                    </div>
+
+                </div>
+            </div>
+
+            <div className="tree">
+                {
+                    constructTree(treeNode.root)
+                }
+                {treeNode.root === null && <p id="emptyTreeMsg">Tree is Empty. Try adding a node.</p>}
+            </div>
         </div>
-
-      </div>
-
-      <div className="tree">
-        {
-          constructTree(treeNode.root)
-        }
-        {treeNode.root === null && <p id="emptyTreeMsg">Tree is Empty. Try adding a node.</p>}
-      </div>
-    </div>
-  );
+    );
 }
 
 export default Tree;
